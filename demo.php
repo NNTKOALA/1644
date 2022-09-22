e71184921aeb1390577719962f4ca01404a097a4
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-09-22 16:22:56 +0700

    php

---
+++ demo.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Demo Web</h1>";
+echo "<h2>Here the page you test code</h2>";
+echo "<h3>Welcome to the demo web page.</h3>";
---
+++ web.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1 style='color: red'>Hello World!</h1>";
+echo "<h2 style='color: green'>Deploy code to Heroku successfully</h2>";
+echo "<h3>Welcome to the web page.</h3>";
+    
+    
