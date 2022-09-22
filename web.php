3609873d61ef42fff03bd96824b85353ac96ceed
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-09-22 16:40:52 +0700

    php

---
+++ web.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Vietnam</title>
+</head>
+<body>
+   <h1>Vietnam is a peaceful country !</h1>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/6zRitbEYe9c" title="YouTube video player"
+      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe>
+   <img
+      src="https://a.cdn-hotels.com/gdcs/production144/d1394/a304783d-6dc9-4bb2-9239-d124a16a154e.jpg?impolicy=fcrop&w=800&h=533&q=medium"
+      alt="">
+   <img src="images/vietnam.png" alt="">
+</body>
+</html
